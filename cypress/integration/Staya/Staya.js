d28f54c5365416e8b2a8c59d7708b4a73f9319f3
Alexandra <s.putivskaya@gmail.com>
2022-12-01 17:49:11 +0300

    changed login/password

--- cypress/integration/Staya/Staya.js
+++ cypress/integration/Staya/Staya.js
@@ -2,8 +2,8 @@ describe('Проверка формы логин/пароль', function () {
     it('Правильный логин/пароль', function () {
         cy.visit('https://staya.dog/');
         cy.get('.header-bottom__right--link').click();
-        cy.get('.auth-form > form > [type="email"]').type('jaijen@mail.ru');
-        cy.get('.auth-form > form > [type="password"]').type('Vdjsabtsofjdnsv');
+        cy.get('.auth-form > form > [type="email"]').type('Верный логин');
+        cy.get('.auth-form > form > [type="password"]').type('Верный пароль');
         cy.get('.auth-form__submit').click();
         cy.contains('Мои заказы');
         cy.get('button.profile__aside--link').click();
@@ -13,8 +13,8 @@ describe('Проверка формы логин/пароль', function () {
      it('Верный логин/ неверный пароль', function () {
         cy.visit('https://staya.dog/');
         cy.get('.header-bottom__right--link').click();
-        cy.get('.auth-form > form > [type="email"]').type('jaijen@mail.ru');
-        cy.get('.auth-form > form > [type="password"]').type('Vdjsabtsofj45dnsv');
+        cy.get('.auth-form > form > [type="email"]').type('Верный логин');
+        cy.get('.auth-form > form > [type="password"]').type('Неверный пароль');
         cy.get('.auth-form__submit').click();
         cy.contains('Невозможно войти с предоставленными учетными данными')
      })
